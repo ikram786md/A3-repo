72ffb3e713eb2af7d1f9a8e04af492684d550e3d
irfan <irfan@DESKTOP-L3R24BG>
2020-03-26 05:36:45 -0700

    my first commit

---
+++ Day1-Demo/src/main/java/org/cap/boot/MainClass.java
@@ -0,0 +1,15 @@
+package org.cap.boot;
+
+import org.cap.model.Sales;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Sales obj=new Sales();
+		obj.print();
+		
+
+	}
+
+}
